a2de7ae44881660f9f51a4db90c1f956e7dc55f3
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-29 09:56:07 -0500

    got server to work

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3006
+    port: 3306
   });
 }
 
